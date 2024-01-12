c5a2a0a92fde680c6c3ccd64c9b30f3bd80124d9
Kenneth V. Domingo <hello@kvd.studio>
2024-01-12 23:42:40 +0800

    fix: use manifest repo name

--- main.sh
+++ main.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
+set -e
+
 echo -e "${PLUGIN_SSH_KEY}" > ~/.ssh/gtkey
 chmod 400 ~/.ssh/gtkey
 echo -e "${PLUGIN_KNOWN_HOSTS}" > ~/.ssh/known_hosts
 echo -e 'Host git.lab.kvd.studio\n  Port 3322\n  IdentityFile ~/.ssh/gtkey\n' > ~/.ssh/config
 
-git clone "git@git.lab.kvd.studio:r/${PLUGIN_REPO_NAME}.git"
-cd "${PLUGIN_REPO_NAME}"
+git clone "git@git.lab.kvd.studio:r/${PLUGIN_MANIFEST_REPO_NAME}.git"
+cd "${PLUGIN_MANIFEST_REPO_NAME}" || exit 1
 yq eval "${PLUGIN_YAML_IMAGE_TAG_PATH} = \"${PLUGIN_IMAGE_TAG}\"" --inplace "${PLUGIN_HELM_VALUES_PATH}"
 
 git add "${PLUGIN_HELM_VALUES_PATH}"
