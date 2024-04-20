e7999d59422d8070360c8a85e963fed09ed47104
Kenneth V. Domingo <hello@kvd.studio>
2024-04-21 03:29:21 +0800

    fix: check if cloned repo already exists

--- main.sh
+++ main.sh
@@ -7,7 +7,10 @@ chmod 400 ~/.ssh/gtkey
 echo -e "${PLUGIN_KNOWN_HOSTS}" > ~/.ssh/known_hosts
 echo -e 'Host git.lab.kvd.studio\n  Port 3322\n  IdentityFile ~/.ssh/gtkey\n' > ~/.ssh/config
 
-git clone "git@git.lab.kvd.studio:r/${PLUGIN_MANIFEST_REPO_NAME}.git"
+if [ ! -d "${PLUGIN_MANIFEST_REPO_NAME}" ]; then
+  git clone "git@git.lab.kvd.studio:r/${PLUGIN_MANIFEST_REPO_NAME}.git" "${PLUGIN_MANIFEST_REPO_NAME}"
+fi
+
 cd "${PLUGIN_MANIFEST_REPO_NAME}" || exit 1
 yq eval "${PLUGIN_YAML_IMAGE_TAG_PATH} = \"${PLUGIN_IMAGE_TAG}\"" --inplace "${PLUGIN_HELM_VALUES_PATH}"
 
